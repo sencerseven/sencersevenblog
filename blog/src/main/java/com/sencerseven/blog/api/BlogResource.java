63a3a0e0718005d5a8c475875003e9124d4785b2
sencerseven <sencerseven@gmail.com>
2018-03-23 16:50:10 +0300

    Spring Boot API

---
+++ blog/src/main/java/com/sencerseven/blog/api/BlogResource.java
@@ -0,0 +1,28 @@
+package com.sencerseven.blog.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sencerseven.blogbackend.dto.Category;
+import com.sencerseven.blogbackend.dto.Images;
+import com.sencerseven.blogbackend.service.CategoryService;
+import com.sencerseven.blogbackend.service.ImagesService;
+
+@RestController
+@RequestMapping("/api/v1/blog")
+public class BlogResource {
+
+	@Autowired
+	ImagesService imageService;
+	
+	@GetMapping(value ="/category/{id}",produces=MediaType.APPLICATION_JSON_VALUE)
+	public Images getCategory(@PathVariable("id")int id){
+		return imageService.getImages(1);		
+	}
+	
+}
---
+++ blogapi/src/test/java/com/sencerseven/blogapi/SencersevenBlogApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sencerseven.blogapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SencersevenBlogApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
